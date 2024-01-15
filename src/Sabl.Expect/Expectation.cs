f100b58afadd00b8ee3a2f0baf005275bceebb31
Joshua Honig <joshua@mallowfields.com>
2024-01-15 13:36:14 -0500

    chore: initialize project and solution

---
+++ src/Sabl.Expect/Expectation.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 2024 Joshua Honig. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+namespace FluentAssertions.Expectations;
+
+/// <summary>
+/// Container for all overloads of Expect(...)
+/// </summary>
+[DebuggerNonUserCode]
+public static partial class Expectation { }
+
+[DebuggerNonUserCode]
+public class Expectation<T>(T subject)
+{
+    public T Subject { get; } = subject;
+}
---
+++ tests/Sabl.Expect.Specs/ExpectationSpecs.cs
@@ -0,0 +1,15 @@
+﻿// Copyright 2024 Joshua Honig. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+namespace FluentAssertions.Expectations.Specs;
+
+public class ExpectationSpecs
+{
+    [Fact]
+    public void Ctor_Sets_Subject()
+    {
+        var exp = new Expectation<int>(2);
+
+        exp.Subject.Should().Be(2);
+    }
+}
