a018fa8bf141c819669ed850bd991c3dab0489c6
Joshua Honig <joshua@mallowfields.com>
2024-05-17 11:31:02 -0400

    feat(string): string assertions

---
+++ src/FluentAssertions.Expectations/StringExpectations.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2024 Joshua Honig. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+using FluentAssertions.Primitives;
+
+namespace FluentAssertions.Expectations;
+
+public static partial class Expectation
+{
+    /// <summary>Create an expectation about a <see cref="string"/> subject</summary>
+    public static Expectation<string> Expect(string? actual) => new(actual!);
+}
+
+/// <summary>Extensions to <see cref="Expectation{T}"/> about a string</summary>
+[DebuggerNonUserCode]
+public static class StringExpectationExtensions
+{
+    /*
+     * See Should() overload:
+     * 
+     * string : https://github.com/fluentassertions/fluentassertions/blob/6.12.0/Src/FluentAssertions/AssertionExtensions.cs#L749
+     */
+
+    /// <summary>Compose assertions about the <see cref="string"/> subject</summary>
+    public static StringAssertions To(this Expectation<string> expectation)
+       => expectation.Subject.Should();
+}
---
+++ tests/FluentAssertions.Expectations.Specs/StringExpectationsSpecs.cs
@@ -0,0 +1,26 @@
+﻿// Copyright 2024 Joshua Honig. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+using FluentAssertions.Primitives;
+
+namespace FluentAssertions.Expectations.Specs;
+
+public class StringExpectationsSpecs
+{
+    [Fact]
+    public void Expect_To_Returns_StringAssertions()
+    {
+        string strValue = "hello";
+
+        // Act: We are testing Expect(...).To() itself
+        var assertions = Expect(strValue).To();
+
+        // Assert
+        Expect(assertions).To().BeOfType<StringAssertions>();
+        assertions.Be("hello");
+
+        // Verify API equivalency
+        var shouldResult = strValue.Should();
+        Expect(assertions).To().BeSameAssertionAs(shouldResult);
+    }
+}
