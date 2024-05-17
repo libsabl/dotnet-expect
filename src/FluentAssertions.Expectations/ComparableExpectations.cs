0b55d05d49b82ad7009a092a17c1d1bbe45774f1
Joshua Honig <joshua@mallowfields.com>
2024-05-17 16:35:05 -0400

    feat(comparable): icomparable expectations

---
+++ src/FluentAssertions.Expectations/ComparableExpectations.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions.Numeric;
+
+namespace FluentAssertions.Expectations;
+
+public static partial class Expectation
+{
+    /// <summary>Create an expectation about a <see cref="IComparable{T}"/> subject</summary>
+    public static Expectation<IComparable<T>> Expect<T>(IComparable<T> actual) => new(actual);
+}
+
+/// <summary>Extensions to <see cref="Expectation{T}"/> for <see cref="IComparable{T}"/> values</summary>
+[DebuggerNonUserCode]
+public static class ComparableExpectationExtensions
+{
+    /// <summary>Compose assertions about the <see cref="IComparable{T}"/> subject</summary>
+    public static ComparableTypeAssertions<T> To<T>(this Expectation<IComparable<T>> expectation)
+        => expectation.Subject.Should();
+}
---
+++ tests/FluentAssertions.Expectations.Specs/ComparableExpectationsSpecs.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions.Numeric;
+
+namespace FluentAssertions.Expectations.Specs;
+
+public class ComparableExpectationsSpecs
+{
+    [Fact]
+    public void Expect_To_Returns_ComparableTypeAssertions()
+    {
+        Version versionValue = new(23, 2, 1);
+
+        // Act: We are testing Expect(...).To() itself
+        var assertions = Expect(versionValue).To();
+
+        // Assert
+        Expect(assertions).To().BeAssignableTo<ComparableTypeAssertions<Version>>();
+        assertions.Be(new(23, 2, 1));
+
+        // Verify API equivalency
+        var shouldResult = versionValue.Should();
+        Expect(assertions).To().BeSameAssertionAs(shouldResult);
+    } 
+}
