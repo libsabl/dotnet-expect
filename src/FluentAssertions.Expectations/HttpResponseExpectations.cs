fc0c1278d234e22e6b3d992fb9943792f348a8f9
Joshua Honig <jhonig@servicetitan.com>
2024-10-28 10:02:24 -0400

    feat(http): httpresponse expectations

---
+++ src/FluentAssertions.Expectations/HttpResponseExpectations.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions.Primitives;
+using System.Net.Http;
+
+namespace FluentAssertions.Expectations;
+
+/// <summary>Extensions to <see cref="IExpectation{T}"/> about <see cref="HttpResponseMessage"/></summary>
+[DebuggerNonUserCode]
+public static class HttpResponseMessageExpectationExtensions
+{
+    /// <summary>Compose assertions about the <see cref="HttpResponseMessage"/> subject</summary>
+    public static HttpResponseMessageAssertions To(this IExpectation<HttpResponseMessage> expectation)
+        => expectation.Subject.Should();
+}
